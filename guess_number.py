8f7b3350a148843b127cbd87c618961b287f7319
dbekin <dbekin@ozon.ru>
2020-07-04 10:58:21 +0300

    final version

--- guess_number.py
+++ guess_number.py
@@ -1,19 +1,26 @@
-
 import random
 
-answer = random.randint(1, 10)
-guess = -1
-while answer != guess:
-    guess = input('Введите число: ')
-    try:
-        guess = int(guess)
-    except ValueError:
-        print('Где у вас башка? Я вам сказала введите ЧИСЛО! Вы слышите, ЧИСЛО!!!!!!!')
-        continue
+want_more = 'хочу'
 
-    if answer < guess:
-        print('Загаданое число меньше')
-    elif answer > guess:
-        print('Загаданое число больше')
-    else:
-        print('Бинго!!!!')
+while want_more == 'хочу':
+    answer = random.randint(1, 10)
+    guess = -1
+    while answer != guess:
+        guess = input('Введите число: ')
+        try:
+            guess = int(guess)
+        except ValueError:
+            print('Где у вас башка? Я вам сказала введите ЧИСЛО! Вы слышите, ЧИСЛО!!!!!!!')
+            continue
+
+        if answer < guess:
+            print('Загаданое число меньше')
+        elif answer > guess:
+            print('Загаданое число больше')
+        else:
+            print('Бинго!!!!')
+            want_more = input('Хотите сыграть ещё? "хочу" или "не хочу": ')
+
+print()
+print('До свиданья(не в том смысле!), приходите ещё')
+print()
