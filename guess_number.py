9649fe75bcb12469c2721c905af79cca8fa2aa94
dbekin <dbekin@ozon.ru>
2020-07-04 10:19:16 +0300

    ver 1

--- guess_number.py
+++ guess_number.py
@@ -1,2 +1,19 @@
 
-print('Hello world')
+import random
+
+answer = random.randint(1, 10)
+guess = -1
+while answer != guess:
+    guess = input('Введите число: ')
+    try:
+        guess = int(guess)
+    except ValueError:
+        print('Где у вас башка? Я вам сказала введите ЧИСЛО! Вы слышите, ЧИСЛО!!!!!!!')
+        continue
+
+    if answer < guess:
+        print('Загаданое число меньше')
+    elif answer > guess:
+        print('Загаданое число больше')
+    else:
+        print('Бинго!!!!')
